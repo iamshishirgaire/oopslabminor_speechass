e9c16c04abe6eb2532024161b263f33256f29f91
iamshishirgaire <shishirgaire35@gmail.com>
2023-02-23 19:05:31 +0545

    string

--- src/core/utils/file_operations.cpp
+++ src/core/utils/file_operations.cpp
@@ -13,30 +13,38 @@ public:
         string line;
         getline(File, line);
     }
-    void writeFile(string fileName ,json content)
+    void writeFile(string fileName, json content)
     {
         ofstream infile(fileName);
-        infile<<content;
+        infile << content;
         infile.close();
     }
-    static ifstream  openFile(const string& fileName){
-        ifstream file(fileName);;
+    static ifstream openFile(const string &fileName)
+    {
+        ifstream file(fileName);
+        ;
         return file;
     }
-    static ifstream  openFile(const string& fileName){
-        ifstream file(fileName);;
+    static ifstream openFile(const string &fileName)
+    {
+        ifstream file(fileName);
+
         return file;
     }
-    void closeFile(ofstream& file){
+    void closeFile(ofstream &file)
+    {
         file.close();
     }
-    void closeFile(ifstream& file){
+    void closeFile(ifstream &file)
+    {
         file.close();
     }
-     vector<json> readEntireFile(ifstream& file){
+    vector<json> readEntireFile(ifstream &file)
+    {
         string line;
         vector<json> jsonData;
-        while(getline(file, line)){
+        while (getline(file, line))
+        {
             auto jsonDat = json::parse(line);
             jsonData.push_back(jsonDat);
         }
