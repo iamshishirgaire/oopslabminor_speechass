8d90ce94e28104226e6f5e6831132711b592b3d8
sudip <sudipthakur643@gmail.com>
2023-02-23 18:59:44 +0545

    file operations

--- src/core/utils/file_operations.cpp
+++ src/core/utils/file_operations.cpp
@@ -1,27 +1,45 @@
 #include "string"
-#include "fstream"
+#include <fstream>
+#include "../../../ext_lib/json.hpp"
 
+using json = nlohmann::json;
+using namespace nlohmann::literals;
+using namespace std;
 class FileOperations
 {
 public:
-    void ReadFile(string fileName)
+    void readOneLine(ofstream File)
     {
-        // todo
+        string line;
+        getline(File, line);
     }
-
-    void writeFile()
+    void writeFile(string fileName ,json content)
     {
-        // todo
+        ofstream infile(fileName);
+        infile<<content;
+        infile.close();
     }
-
-    void getOneLine()
-    {
-        // todo
+    static ifstream  openFile(const string& fileName){
+        ifstream file(fileName);;
+        return file;
     }
-
-    void writeOneLine()
-    {
-
-        // todo
+    static ifstream  openFile(const string& fileName){
+        ifstream file(fileName);;
+        return file;
+    }
+    void closeFile(ofstream& file){
+        file.close();
+    }
+    void closeFile(ifstream& file){
+        file.close();
+    }
+     vector<json> readEntireFile(ifstream& file){
+        string line;
+        vector<json> jsonData;
+        while(getline(file, line)){
+            auto jsonDat = json::parse(line);
+            jsonData.push_back(jsonDat);
+        }
+        return jsonData;
     }
 }
