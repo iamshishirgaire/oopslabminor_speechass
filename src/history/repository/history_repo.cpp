92939d4605d32ec8fe7c65bdee5328285f37b437
iamshishirgaire <shishirgaire35@gmail.com>
2023-02-23 20:51:29 +0545

    history

--- src/history/repository/history_repo.cpp
+++ src/history/repository/history_repo.cpp
@@ -1,3 +1,26 @@
+#include "vector"
+#include "../../../ext_lib/json.hpp"
+#include "string"
+#include "../../core/utils/file_operations.cpp"
+using namespace std;
+using json = nlohmann::json;
+
 class HistoryRepo
 {
+
+    FileOperations fos;
+    vector<json> getHistory(string userName)
+    {
+        return fos.readEntireFile()
+    }
+
+    void postHistory(string userName, json assessmentResult)
+    {
+
+        json jsonData;
+        jsonData["userName"] = userName;
+        jsonData["assessmentResult"] = assessmentResult;
+    }
+
+public:
 }
