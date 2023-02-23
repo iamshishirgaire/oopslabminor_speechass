5f46b90b21ccd0a7e7ca979d3f29ee5ef7524c0a
iamshishirgaire <shishirgaire35@gmail.com>
2023-02-23 20:51:39 +0545

    lol

--- src/history/model/history_model.cpp
+++ src/history/model/history_model.cpp
@@ -1,3 +1,28 @@
+
+
+#include "../../../ext_lib/json.hpp"
+#include "string"
+using namespace std;
+using json = nlohmann::json;
+
 class History
 {
+
+public:
+    string userName;
+    json assessmentResult;
+
+    json toJson()
+    {
+        json jsonData;
+        jsonData["userName"] = userName;
+        jsonData["assessmentResult"] = assessmentResult;
+        return jsonData;
+    }
+
+    History(json jsonData)
+    {
+        userName = jsonData["userName"];
+        assessmentResult = jsonData["assessmentResult"];
+    }
 }
